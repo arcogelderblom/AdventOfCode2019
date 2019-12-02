aa24c84198892311124b62207e80a10c4fa740de
Arco Gelderblom <arco.gelderblom@hotmail.com>
2019-12-02 23:05:38 +0100

    Updated gitignore and added solution for day 2 puzzle 1

--- 02-december/main.cpp
+++ 02-december/main.cpp
@@ -1,5 +1,20 @@
 #include "OpcodeCalculator.hpp"
+#include "FileLoader.hpp"
+
+#include <iostream>
 
 int main() {
+    OpcodeCalculator calculator;
+    FileLoader fileLoader;
+    std::vector<std::string> codes = fileLoader.getCommaSeparatedVectorFromFile("02-december/input.txt");
+    std::vector<int> opcode;
+    for (std::string code : codes) {
+        opcode.push_back(std::stoi(code));
+    }
+
+    // initial setup
+    opcode[1] = 12;
+    opcode[2] = 2;
+    std::cout << "Result for puzzle one: " << calculator.calculateOpcode(opcode, 0) << std::endl;
     return 0;
 }
